458c15e4ad5dc752ae7cfd168ae51947f42bb408
johan prueba <yepmarquez@gmail.com>
2026-03-18 21:12:06 -0600

    agregar calculadora multip

--- calculadora.js
+++ calculadora.js
@@ -6,4 +6,12 @@ function restar(a, b) {
     return a - b;
 }
 sumar(1, 2);
-restar(1, 2); 
+restar(1, 2);
+
+function multiplicar(a, b) {
+    return a * b;
+}
+
+function dividir(a, b) {
+    return a / b;
+}        
